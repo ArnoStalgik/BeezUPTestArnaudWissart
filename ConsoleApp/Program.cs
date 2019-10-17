a3c72fa45d7525be3cf64a684f4553d74d31d2e7
Arnaud Wissart <arnaud.wissart@live.fr>
2019-10-17 17:28:25 +0200

    Part 1 / Step 1
    - Ajout du nuget CSVHelper
    - Ajout du fichier CSV à la solution pour faciliter les tests
    - Mise en place de la routine de lecture de fichier afin de répondre à la demande Part 1 / Step 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CsvHelper;
+using System;
+using System.IO;
 
 namespace ConsoleApp
 {
     class Program
     {
+        private const string CSVFILENAME = "bigfile.csv";
         static void Main(string[] args)
         {
+            using (var reader = new StreamReader(CSVFILENAME))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                string columnA, columnB;
+                int columnC, columnD;
+                while (csv.Read())
+                {
+                    if (csv.TryGetField<int>("columnC", out columnC))
+                    {
+                        if (csv.TryGetField<int>("columnD", out columnD))
+                        {
+                            if (columnC + columnD > 100)
+                            {
+                                columnA = csv.GetField<string>("columnA");
+                                columnB = csv.GetField<string>("columnB");
+                                Console.WriteLine(columnA + columnB);
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
